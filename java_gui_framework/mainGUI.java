0d4d625b6a6a55ad74174d83f401bba162e93536
Dilesh14 <43584762+Dilesh14@users.noreply.github.com>
2018-11-26 23:51:10 -0330

    Update mainGUI.java

--- java_gui_framework/mainGUI.java
+++ java_gui_framework/mainGUI.java
@@ -1,18 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class mainGUI extends JFrame implements ActionListener {
 	static final String APP_NAME = "Activity Tracker";
 	JPanel cardManager;
+	panelLogin cardLogin = new panelLogin();
+	panelRegister cardRegister = new panelRegister();
+	panelForgotPassword cardForgotPassword = new panelForgotPassword();
 
 	mainGUI(){
 		super(APP_NAME);
 
-		panelLogin cardLogin = new panelLogin();
-		panelRegister cardRegister = new panelRegister();
-		panelForgotPassword cardForgotPassword = new panelForgotPassword();
-		cardManager = new JPanel(new CardLayout());
+				cardManager = new JPanel(new CardLayout());
 
 		cardManager.add(cardLogin, "login");
 		cardManager.add(cardRegister, "register");
@@ -24,11 +26,13 @@ public class mainGUI extends JFrame implements ActionListener {
 		cardLogin.getButtonRegister().setActionCommand("showCardRegister");
 		cardLogin.getButtonForgotPassword().addActionListener(this);
 		cardLogin.getButtonForgotPassword().setActionCommand("showCardForgotPassword");
+		
+		cardRegister.getButtonRegister().addActionListener(this);
+		cardRegister.getButtonRegister().setActionCommand("showCardReg");
 		cardRegister.getButtonBack().addActionListener(this);
 		cardRegister.getButtonBack().setActionCommand("showCardLogin");
 		cardForgotPassword.getButtonBack().addActionListener(this);
 		cardForgotPassword.getButtonBack().setActionCommand("showCardLogin");
-
 		this.getContentPane().add(cardManager);
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -36,19 +40,28 @@ public class mainGUI extends JFrame implements ActionListener {
 		setVisible(true);
 	}
 
-	public void actionPerformed(ActionEvent e){
+	public void actionPerformed(ActionEvent e) {
 		if(e.getActionCommand().equals("showCardRegister")){
-			setSize(450, 650);
 			CardLayout cards = (CardLayout)(cardManager.getLayout());
 			cards.show(cardManager, "register");
 		}
+		if(e.getActionCommand().equals("showCardReg")){
+			try {
+				saveusername();
+				//savepassword();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			CardLayout cards = (CardLayout)(cardManager.getLayout());
+			cards.show(cardManager, "login");
+		}
 		if(e.getActionCommand().equals("showCardLogin")){
-			setSize(450, 650);
+
 			CardLayout cards = (CardLayout)(cardManager.getLayout());
 			cards.show(cardManager, "login");
 		}
 		if(e.getActionCommand().equals("showCardForgotPassword")){
-			setSize(450, 650);
 			CardLayout cards = (CardLayout)(cardManager.getLayout());
 			cards.show(cardManager, "forgotpassword");
 		}
@@ -57,9 +70,29 @@ public class mainGUI extends JFrame implements ActionListener {
 			CardLayout cards = (CardLayout)(cardManager.getLayout());
 			cards.show(cardManager, "forgotpassword");
 			*/
-			setSize(800, 800);
 		}
 	}
+	public void saveusername() throws IOException {
+		FileWriter usernamefile = new FileWriter("C:\\Users\\Dilesh\\Documents\\Assignments$\\FitnessTracker\\src\\database.txt",true);
+		String containp = cardRegister.getPass();
+		String contain = cardRegister.gettext(); 
+		String conpassw = cardRegister.getConpass();
+		if (containp.equals(conpassw)) {
+		usernamefile.write(System.lineSeparator()+contain+","+containp);}
+		else {System.out.println("Different password");}
+		System.out.println("*****");
+		
+		usernamefile.close();
+		
+	}
+//	public void savepassword() throws IOException {
+//		FileWriter passwordfile = new FileWriter("C:\\Users\\Dilesh\\Documents\\Assignments$\\FitnessTracker\\src\\password.txt",true);
+//		String containp = cardRegister.getpass(); 
+//		passwordfile.write(System.lineSeparator()+containp);
+//		System.out.println("*****");
+//		passwordfile.close();
+//		
+//	}
 
 	public static void main(String args[]){
 		new mainGUI();
